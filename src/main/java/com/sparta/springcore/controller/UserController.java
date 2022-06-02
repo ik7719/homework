da9fef2c03531c44b073978d1f47efc747ad9b0e
ik7719 <105044987+ik7719@users.noreply.github.com>
2022-06-02 18:24:40 +0900

    제출 전 저장

--- src/main/java/com/sparta/springcore/controller/UserController.java
+++ src/main/java/com/sparta/springcore/controller/UserController.java
@@ -6,16 +6,20 @@ import com.sparta.springcore.model.UserRoleEnum;
 import com.sparta.springcore.security.UserDetailsImpl;
 
 import com.sparta.springcore.service.UserService;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Controller
+@Slf4j
+@Getter
+@RestController
 public class UserController {
 
     private final UserService userService;
--- src/main/java/com/sparta/springcore/service/UserService.java
+++ src/main/java/com/sparta/springcore/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
@@ -22,6 +23,7 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Transactional
     public void registerUser(SignupRequestDto requestDto) {
         // 회원 ID 중복 확인
 
