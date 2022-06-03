2961b88769eeefc753633efbc24128520c3ebab6
ik7719 <105044987+ik7719@users.noreply.github.com>
2022-06-03 09:35:37 +0900

    5주차 과제 시작

---
+++ src/main/java/com/homework/start0603/Start0603Application.java
@@ -0,0 +1,13 @@
+package com.homework.start0603;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start0603Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start0603Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/homework/start0603/Start0603ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.start0603;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Start0603ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
