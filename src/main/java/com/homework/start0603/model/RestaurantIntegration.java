6e10d07bf4480e3e8d3fc4602d2097bbebe2eaea
ik7719 <105044987+ik7719@users.noreply.github.com>
2022-06-03 09:46:55 +0900

    restaurantIntegration field write

--- src/main/java/com/homework/start0603/model/RestaurantIntegration.java
+++ src/main/java/com/homework/start0603/model/RestaurantIntegration.java
@@ -1,4 +1,22 @@
 package com.homework.start0603.model;
 
-public class RestaurantIntegration {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class RestaurantIntegration
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // 음식점 이름
+
+    @Column(nullable = false)
+    private int minOrderPrice; // 최소주문 가격
+
+    @Column(nullable = false)
+    private int deliveryFee; // 기본 배달비
 }
